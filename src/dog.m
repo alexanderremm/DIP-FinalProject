4b32bab3b8a04b7c7ca41c4980c4ca895f44c013
remman <remman@mail.uc.edu>
2022-04-14 11:34:30 -0400

    Skeleton code for DoG method

---
+++ src/dog.m
@@ -0,0 +1,9 @@
+function [dog_img] = dog(img)
+%DOG Function to compute Difference of Gaussians saliency map
+%   Computes the Difference of Gaussians saliency map based on the 
+% following paper: Real-Time Visual Saliency Detection
+% Using Gaussian Distribution (Wang et al.)
+
+
+end
+
