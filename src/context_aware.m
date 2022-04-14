50c7c17f2367019573318396b434a22926ea042c
remman <remman@mail.uc.edu>
2022-04-14 11:19:32 -0400

    Skeleton code for Context Aware method

---
+++ src/context_aware.m
@@ -0,0 +1,9 @@
+function [ca_image] = context_aware(img)
+%CONTEXT AWARE Function to compute Context Aware saliency map
+%   Computes the Context Aware saliency map based on the following
+%   paper: Context-Aware Saliency Detection (Goferman et al.)
+
+
+
+end
+
